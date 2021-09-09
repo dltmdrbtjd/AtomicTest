391b813c1051b604339b1675550db1b4a1556303
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-09 21:54:33 +0900

    organisms/mainItem main item image 추가

---
+++ test-app/src/components/organisms/MainItem/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './style'
+
+function MainItem(){
+  return (
+    <S.ItemImage src='https://eizer.kr/data/goods/1/2020/12/774_tmp_fbb5431411e4c9b3a2a14c40849a66cb6463view.jpg'/>
+  )
+}
+
+export default MainItem
---
+++ test-app/src/components/organisms/MainItem/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import Img from '../../atoms/Image'
+
+export const ItemImage = styled(Img)`
+  width: 360px;
+  height: 360px;
+`
