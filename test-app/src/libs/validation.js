7aa4eb962cc46fd6dd40c703e2965a082e29f709
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-09 21:58:06 +0900

    패스워드 검증 함수 불필요로 인한 삭제

--- test-app/src/libs/validation.js
+++ test-app/src/libs/validation.js
@@ -1,9 +1,1 @@
 export const EmailRegExp = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/
-
-export const PwCheck = (pw) => {
-  if (pw.length > 8) {
-    return true
-  } else {
-    return false
-  }
-}
